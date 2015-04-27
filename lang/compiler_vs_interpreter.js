2b10091beb9ec26525abe1e73fc91fd88387d8cf
James Zhan <zhiqiangzhan@gmail.com>
2015-04-27 19:17:37 +0800

    rewrite lang/compiler_vs_interpreter.js

--- lang/compiler_vs_interpreter.js
+++ lang/compiler_vs_interpreter.js
@@ -1,83 +1,122 @@
-function isNumber(str) {
-    return /^\d+$/.test(str);
-}
+// Expression ::= Term | Term '+|-' Expression
+// Term ::= Primary | Primary '*|/' Term
+// Primary ::= Number | '(' Expression ')'
 
-function isOperator(str) {
-    return /^[-+*/]$/.test(str);
-}
-
-function buildAst(tokens) {
-    var sym, left, right, token, stack = [];
-    while (token = tokens.shift()) {
-        if (isNumber(token)) {
-            sym = {value: token, type: 'number'};
-            stack.push(sym);
-        } else if (isOperator(token)) {
-            left = stack.pop();
-            if (left) {
-                sym = {children: [left], value: token, type: 'operator'};
-            } else {
-                console.error('Syntax error at: ' + left);
-                throw 'Syntax error at: ' + left;
-            }
-            right = tokens.shift();
-            if (right && isNumber(right)) {
-                sym.children[1] = {value: right, type: 'number'};
-                stack.push(sym);
-            } else {
-                console.error('Syntax error at: ' + right);
-                throw 'Syntax error at: ' + right;
-            }
-        }
+function parseExpression(tokens) {
+    var v1, v2, token;
+    v1 = parseTerm(tokens);
+    token = tokens.shift();
+    console.log("v1 in expression: ", v1, ", token: ", token);
+    if (token === undefined) {
+        return v1;
+    } else if (token.type === 'plus') {
+        v2 = parseExpression(tokens);
+        return {type: 'op', value: token.value, v1: v1, v2: v2};
+    } else {
+        console.warn("token in experssion: ", token);
+        tokens.unshift(token);
+        return v1;
     }
-    return stack.pop();
+}
+
+function parseTerm(tokens){
+    var v1, v2, token;
+    v1 = parsePrimary(tokens);
+    token = tokens.shift();
+    if (token === undefined) {
+        return v1;
+    } else if (token.type === 'mul') {
+        v2 = parseTerm(tokens);
+        return {type: 'op', value: token.value, v1: v1, v2: v2};
+    } else {
+        console.warn("token in term: ", token);
+        tokens.unshift(token);
+        return v1;
+    }
+}
+
+function parsePrimary(tokens) {
+    var token = tokens.shift(), value;
+    if (token === undefined) {
+        throw new SyntaxError("Primary can't be null.");
+    } else if (token.type === 'number') {
+        return token.value;
+    } else if (token.type === '(') {
+        value = parseExpression(tokens);
+        token = tokens.shift();
+        if (token === undefined || token.type !== ')') {
+            throw new SyntaxError("unclosed delimeter till end of file: " + token);
+        }
+        return value;
+    } else {
+        throw new SyntaxError("Unknown token " + JSON.stringify(token) + " in Primary.");
+    }
 }
 
 
 function parse(code) {
     var tokens = code.split('"').map(function(x, i){
         if (i % 2 === 0){ // not in string
-            return x.replace(/([-+*/])/g, " $1 ");
+            return x.replace(/([-+*/()])/g, " $1 ");
         } else { // in string
             return x.replace(/\s/g, "#whitespace#");
         }
     }).join('"').trim().split(/\s+/).map(function(x){
         return x.replace(/#whitespace#/g, " ");
+    }).map(function(input, i) {
+        if (input === '(') {
+            return {type: '(', value: '('};
+        } else if (input === ')') {
+            return {type: ')', vaue: ')'};
+        } else if (input === '+' || input === '-') {
+            return {type: 'plus', value: input};
+        } else if (input === '*' || input === '/') {
+            return {type: 'mul', value: input};
+        } else if (!isNaN(parseFloat(input))) {
+            return {type: 'number', value: parseFloat(input)};
+        } else {
+            throw new SyntaxError("Unknow token: " + input);
+        }
     });
-    return buildAst(tokens);
+    console.log(tokens);
+    return parseExpression(tokens);
 }
 
-function compile(node) {
-    switch (node.type) {
-        case 'number':
-            return "ds.push(" + node.value + ")\n";
-        case 'operator':
-            return compile(node.children[0]) + compile(node.children[1]) + "ds.push(ds.pop() " + node.value + " ds.pop())\n";
-        default:
-            return "Unknown Node: " + node;
-    }
-}
+var ast = parse("(1 + 2)");
+//var ast = parse("1 + 2 * 3)");
+console.log(ast);
 
-var ast = parse('1+2-3+4-5+6');
-var code = compile(ast);
-console.log(code);
-eval("var ds = [];" + code);
-
-
-function interpret(node) {
-    switch (node.type) {
-        case 'number':
-            return +node.value;
-        case 'operator':
-            if (node.value == '+') {
-                return interpret(node.children[0]) + interpret(node.children[1]);
-            } else {
-                return interpret(node.children[0]) - interpret(node.children[1]);
-            }
-        default:
-            return "Unknown Node: " + node;
-    }
-}
-
-var val = interpret(ast);
-console.log(val);
+//function compile(node) {
+//    switch (node.type) {
+//        case 'number':
+//            return "ds.push(" + node.value + ")\n";
+//        case 'operator':
+//            return compile(node.children[0]) + compile(node.children[1]) + "ds.push(ds.pop() " + node.value + " ds.pop())\n";
+//        default:
+//            return "Unknown Node: " + node;
+//    }
+//}
+//
+//var ast = parse('1+2-3+4-5+6');
+//var code = compile(ast);
+//console.log(code);
+//eval("var ds = [];" + code);
+//
+//
+//function interpret(node) {
+//    switch (node.type) {
+//        case 'number':
+//            return +node.value;
+//        case 'operator':
+//            if (node.value == '+') {
+//                return interpret(node.children[0]) + interpret(node.children[1]);
+//            } else {
+//                return interpret(node.children[0]) - interpret(node.children[1]);
+//            }
+//        default:
+//            return "Unknown Node: " + node;
+//    }
+//}
+//
+//var val = interpret(ast);
+//console.log(val);
